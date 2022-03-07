9c85740b6d5d9f70cae6fa54757b38e073d8843d
T4G0Rr <ernestas.baltrusis@students.plymouth.ac.uk>
2022-03-08 01:03:17 +0200

    Map functionality update
    > Added 3 sniper AI's which are clones of enemy AI
    > Fixed previous compiling errors
    > Edited MapBehaviour script to automatically create markers and track them via tags
    > Created Resources\Material
    > Created 3 color materials for map markers
    > Moved map on PC screen

--- Photon/Assets/Scripts/Actor/HostageAi.cs
+++ Photon/Assets/Scripts/Actor/HostageAi.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HostageAi : MonoBehaviour
+public class HostageAI : MonoBehaviour
 {
     bool attention = false;
     Transform user;
--- Photon/Assets/Scripts/World/MapBehaviour.cs
+++ Photon/Assets/Scripts/World/MapBehaviour.cs
@@ -3,33 +3,98 @@ using UnityEngine;
 public class MapBehaviour : MonoBehaviour
 {
     [SerializeField] private GameObject _groundPlane;
-    [SerializeField] private GameObject _mapPlane;
-    [SerializeField] private GameObject _playerController;
-    [SerializeField] private GameObject _playerMapMarker;
-
-    [SerializeField] private Color _playerMapMarkerColor = Color.green;
+    [SerializeField] private Material _greenMaterial;
+    [SerializeField] private Material _redMaterial;
+    [SerializeField] private Material _blueMaterial;
+    private GameObject _playerMapMarker;
+    private GameObject _enemyMapMarker;
+    private GameObject _sniper1MapMarker;
+    private GameObject _sniper2MapMarker;
+    private GameObject _sniper3MapMarker;
+    private GameObject _playerController;
+    private GameObject _enemyController;
+    private GameObject _sniper1Controller;
+    private GameObject _sniper2Controller;
+    private GameObject _sniper3Controller;
 
     // Start is called before the first frame update
     private void Start()
     {
-        var _cylinderRenderer = _playerMapMarker.GetComponent<Renderer>();
-        _cylinderRenderer.material.SetColor("_Color", _playerMapMarkerColor);
+        _playerController = GameObject.FindGameObjectWithTag("Player");
+        _enemyController = GameObject.FindGameObjectWithTag("Enemy");
+        _sniper1Controller = GameObject.FindGameObjectWithTag("Sniper1");
+        _sniper2Controller = GameObject.FindGameObjectWithTag("Sniper2");
+        _sniper3Controller = GameObject.FindGameObjectWithTag("Sniper3");
+        _mapMarkerInitialization();
     }
 
     // Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
-        var _xScale = _mapPlane.transform.localScale.x / _groundPlane.transform.localScale.x;
-        if (_xScale < 0) _xScale *= -1;
+        _updatePos(_playerController, _playerMapMarker);
+        _updatePos(_enemyController, _enemyMapMarker);
+        _updatePos(_sniper1Controller, _sniper1MapMarker);
+        _updatePos(_sniper2Controller, _sniper2MapMarker);
+        _updatePos(_sniper3Controller, _sniper3MapMarker);
+    }
 
-        var _yScale = _mapPlane.transform.localScale.y / _groundPlane.transform.localScale.z;
-        if (_yScale < 0) _yScale *= -1;
+    private void _mapMarkerInitialization() 
+    {
+        _playerMapMarker = _mapMarkerCreation("Player");
+        _enemyMapMarker = _mapMarkerCreation("Enemy");
+        _sniper1MapMarker = _mapMarkerCreation("Sniper1");
+        _sniper2MapMarker = _mapMarkerCreation("Sniper2");
+        _sniper3MapMarker = _mapMarkerCreation("Sniper3");
+    }
 
-        var _posX = _mapPlane.transform.position.x + _playerController.transform.position.x * _xScale;
-        var _posY = _mapPlane.transform.position.y - _playerController.transform.position.z * _yScale;
-        var _posZ = _mapPlane.transform.position.z;
+    private GameObject _mapMarkerCreation(string tag)
+    {
+        GameObject _targetObject;
+        Vector3 _defaultPoss = new Vector3(0, 0, 0);
+        var _defaultRot = Quaternion.Euler(0, 0, 0);
+        Vector3 _defaultScale = new Vector3(1f, 0.01f, 1f);
 
-        var _newPoss = new Vector3(_posX, _posY, _posZ);
-        _playerMapMarker.transform.position = _newPoss;
+        _targetObject = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        _targetObject.transform.SetParent(gameObject.transform);
+        _targetObject.transform.localPosition = _defaultPoss;
+        _targetObject.transform.localRotation = _defaultRot;
+        _targetObject.transform.localScale = _defaultScale;
+
+        if (tag == "Player")
+        {
+            _targetObject.name = "PlayerMapMarker";
+            _targetObject.GetComponent<Renderer>().material = _greenMaterial;
+        }
+        else if (tag == "Sniper1")
+        {
+            _targetObject.name = "Sniper1MapMarker";
+            _targetObject.GetComponent<Renderer>().material = _blueMaterial;
+        }
+        else if (tag == "Sniper2")
+        {
+            _targetObject.name = "Sniper2MapMarker";
+            _targetObject.GetComponent<Renderer>().material = _blueMaterial;
+        }
+        else if (tag == "Sniper3")
+        {
+            _targetObject.name = "Sniper3MapMarker";
+            _targetObject.GetComponent<Renderer>().material = _blueMaterial;
+        }
+        else if (tag == "Enemy")
+        {
+            _targetObject.name = "EnemyMapMarker";
+            _targetObject.GetComponent<Renderer>().material = _redMaterial;
+        }
+        return _targetObject;
+    }
+
+    private void _updatePos(GameObject _controller, GameObject _marker)
+    {
+        float _posX = _controller.transform.position.x / _groundPlane.transform.localScale.x;
+        float _posY = 0;
+        float _posZ = _controller.transform.position.z / _groundPlane.transform.localScale.z;
+
+        Vector3 _newPoss = new Vector3(_posX, _posY, _posZ);
+        _marker.transform.localPosition = _newPoss;
     }
 }

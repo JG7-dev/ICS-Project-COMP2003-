b34ce8e7e4dfa41b791a8fb5680b95f94fb512a2
JG7-dev <72551195+JG7-dev@users.noreply.github.com>
2022-03-30 01:22:26 +0100

    Add files via upload

---
+++ Photon/Assets/Scripts/Player/NegotiatiorHP.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NegotiatiorHP
+{
+    private int maxHP = 100;
+    private int currentHP = 100;
+    private bool hit = false;
+    public Slider HealthBar;
+
+    void Start()
+    {
+        currentHP = maxHP;
+    }
+
+    void Update()
+    {
+        if(hit == true)
+        {
+            damageTaken();
+        }
+    }
+
+    void damageTaken()
+    {
+        //HealthBar.value = ;
+    }
+}

d938b3155a65b3bf19d75da95359ed8c07f39370
Jay Redmond <51208129+JTemren@users.noreply.github.com>
2022-04-20 15:45:29 +0100

    Added gravity to the player
    added a simple gravity calculation to the player allowing the player to move on the Y axis correctly

--- Photon/Assets/Scripts/Player/PlayerMotor.cs
+++ Photon/Assets/Scripts/Player/PlayerMotor.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class PlayerMotor : MonoBehaviour
 {
     private CharacterController _controller;
-
+    public bool isGrounded;
     private Vector3 playerVelocity;
+    public float gravity = -9.8f;
 
     private float speed = 5f;
     // Start is called before the first frame update
@@ -18,7 +19,7 @@ public class PlayerMotor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = _controller.isGrounded;
     }
 
     public void ProcessMove(Vector2 input)
@@ -29,5 +30,14 @@ public class PlayerMotor : MonoBehaviour
 
         _controller.Move(transform.TransformDirection(moveDirection * speed * Time.deltaTime));
 
+        playerVelocity.y += gravity * Time.deltaTime;
+        if (isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = -1;
+        }
+        _controller.Move(playerVelocity * Time.deltaTime);
+        Debug.Log(playerVelocity.y);
+        
+        
     }
 }
